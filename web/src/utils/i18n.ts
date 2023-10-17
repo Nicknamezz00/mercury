2f5236ca430f7feb0e6ef4c398c636693c974589
wrz <74159645+Nicknamezz00@users.noreply.github.com>
2023-10-17 20:39:43 +0800

    i18n

--- web/src/utils/i18n.ts
+++ web/src/utils/i18n.ts
@@ -22,7 +22,40 @@
  * SOFTWARE.
  *
  */
-export const findLanguageMatch = (): string => {
-  // TODO: findLanguageMatch
-  return "";
+import { FallbackLng, FallbackLngObjList } from "i18next";
+import { useTranslation } from "react-i18next";
+import i18n, { availableLocales, TLocale } from "@/i18n";
+import locales from "@/locales/en.json";
+import { NestedKeyOf } from "@/types/utils/nestedKeyOf.types";
+
+export const findLanguageMatch = (codename: string): string => {
+  if (codename.length > 2 && availableLocales.includes(codename as TLocale)) {
+    return codename as Locale;
+  }
+
+  const i18nFallbacks = Object.entries(i18n.store.options.fallbackLng as FallbackLng);
+  for (const [main, fallbacks] of i18nFallbacks) {
+    if (codename === main) {
+      return fallbacks[0] as Locale;
+    }
+  }
+
+  const shortCode = codename.substring(0, 2);
+  if (availableLocales.includes(shortCode as TLocale)) {
+    return shortCode as Locale;
+  }
+  for (const exist of availableLocales) {
+    if (shortCode == exist.substring(0, 2)) {
+      return exist as Locale;
+    }
+  }
+  return (i18n.store.options.fallbackLng as FallbackLngObjList).default[0] as Locale;
+};
+
+type Translations = NestedKeyOf<typeof locales>;
+type TypeT = (key: Translations, params?: Record<string, any>) => string;
+
+export const useTranslate = (): TypeT => {
+  const { t } = useTranslation<Translations>();
+  return t;
 };
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from "path"
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-let devProxyServer = "http://localhost:8081/";
+const devProxyServer = "http://localhost:8081/";
 if (process.env.DEV_PROXY_SERVER && process.env.DEV_PROXY_SERVER.length > 0) {
   console.log("Using dev proxy server: ", process.env.DEV_PROXY_SERVER);
 }
@@ -30,11 +30,11 @@ export default defineConfig({
         target: devProxyServer,
         xfwd: true,
       },
-    }
+    },
   },
   resolve: {
     alias: {
       "@/": `${resolve(__dirname, "src")}`,
-    }
-  }
-})
+    },
+  },
+});
